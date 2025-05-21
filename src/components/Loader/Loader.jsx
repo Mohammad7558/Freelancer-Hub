c735898258573a0a06ec8619fdea660010916bad
Mohammad <108111892+Mohammad7558@users.noreply.github.com>
2025-05-21 17:12:45 +0600

    add-loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,7 +5,7 @@ import loader from "../../../src/assets/Loader.json";
 const Loader = () => {
   return (
     <div className="w-full h-[70vh] flex justify-center items-center">
-      <div className="w-40">
+      <div className="w-28">
         <Lottie animationData={loader} loop={true}></Lottie>
       </div>
     </div>
